297e5c2094e4a7c7b732133245cc969f7ffcb888
Chiori Greene <chiorigreene@gmail.com>
2014-05-09 16:55:03 -0500

    Add an InputStream consumer to the FileUtils class

--- server/src/com/chiorichan/util/FileUtil.java
+++ server/src/com/chiorichan/util/FileUtil.java
@@ -1,9 +1,11 @@
 package com.chiorichan.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.channels.FileChannel;
 
 /**
@@ -11,35 +13,53 @@ import java.nio.channels.FileChannel;
  */
 public class FileUtil
 {
+	public static byte[] inputStream2Bytes( InputStream is ) throws IOException
+	{
+		return inputStream2ByteArray( is ).toByteArray();
+	}
+
+	public static ByteArrayOutputStream inputStream2ByteArray( InputStream is ) throws IOException
+	{
+		int nRead;
+		byte[] data = new byte[16384];
+		ByteArrayOutputStream bs = new ByteArrayOutputStream();
+
+		while ( (nRead = is.read( data, 0, data.length )) != -1 )
+		{
+			bs.write( data, 0, nRead );
+		}
+
+		bs.flush();
+
+		return bs;
+	}
+
 	/**
 	 * This method copies one file to another location
-	 * 
+	 *
 	 * @param inFile
-	 *             the source filename
+	 * the source filename
 	 * @param outFile
-	 *             the target filename
+	 * the target filename
 	 * @return true on success
 	 */
-	
 	@SuppressWarnings( "resource" )
 	public static boolean copy( File inFile, File outFile )
 	{
 		if ( !inFile.exists() )
-		{
 			return false;
-		}
-		
+
 		FileChannel in = null;
 		FileChannel out = null;
-		
+
 		try
 		{
 			in = new FileInputStream( inFile ).getChannel();
 			out = new FileOutputStream( outFile ).getChannel();
-			
+
 			long pos = 0;
 			long size = in.size();
-			
+
 			while ( pos < size )
 			{
 				pos += in.transferTo( pos, 10 * 1024 * 1024, out );
@@ -54,20 +74,16 @@ public class FileUtil
 			try
 			{
 				if ( in != null )
-				{
 					in.close();
-				}
 				if ( out != null )
-				{
 					out.close();
-				}
 			}
 			catch ( IOException ioe )
 			{
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
 }
