1ba31a1ccbacedceff8c068a064a3f1ab5cceac6
Chiori Greene <chiorigreene@gmail.com>
2014-06-07 21:33:18 -0500

    Fixes to the FileUserAdapter

--- server/src/com/chiorichan/user/builtin/FileAdapter.java
+++ server/src/com/chiorichan/user/builtin/FileAdapter.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
-import com.chiorichan.configuration.ConfigurationSection;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+
 import com.chiorichan.file.YamlConfiguration;
 import com.chiorichan.framework.Site;
 import com.chiorichan.framework.WebUtils;
@@ -16,12 +19,13 @@ import com.chiorichan.user.User;
 import com.chiorichan.user.UserMetaData;
 import com.chiorichan.util.Common;
 import com.chiorichan.util.FileUtil;
+import com.google.common.collect.Maps;
 
 public class FileAdapter implements UserLookupAdapter
 {
 	File usersDirectory = null;
 	List<String> userFields;
-	YamlConfiguration adapterConfig = new YamlConfiguration();
+	Map<String, UserMetaData> preloaded = Maps.newConcurrentMap();
 	
 	public FileAdapter(Site site) throws LookupAdapterException
 	{
@@ -35,7 +39,40 @@ public class FileAdapter implements UserLookupAdapter
 		userFields.add( "userId" );
 		userFields.add( "username" );
 		
-		adapterConfig = YamlConfiguration.loadConfiguration( new File( usersDirectory, "adapter.yaml" ) );
+		checkForFiles();
+	}
+	
+	public void checkForFiles()
+	{
+		for ( File f : usersDirectory.listFiles() )
+			if ( FileFilterUtils.and( FileFilterUtils.suffixFileFilter( "yaml" ), FileFilterUtils.fileFileFilter() ).accept( f ) )
+				if ( !preloaded.containsKey( f.getName() ) )
+					preloaded.put( f.getName(), loadFromFile( f ) );
+	}
+	
+	public UserMetaData loadFromFile( File absoluteFilePath )
+	{
+		UserMetaData meta = new UserMetaData();
+		
+		if ( !absoluteFilePath.exists() )
+			return null;
+		
+		YamlConfiguration yser = YamlConfiguration.loadConfiguration( absoluteFilePath );
+		
+		if ( yser == null )
+			return null;
+		
+		// Save the file location for later
+		meta.set( "file", absoluteFilePath );
+		
+		for ( String key : yser.getKeys( false ) )
+		{
+			meta.set( key, yser.get( key ) );
+		}
+		
+		// meta.set( "displayName", ( rs.getString( "fname" ).isEmpty() ) ? rs.getString( "name" ) : rs.getString( "fname" ) + " " + rs.getString( "name" ) );
+		
+		return meta;
 	}
 	
 	@Override
@@ -56,7 +93,7 @@ public class FileAdapter implements UserLookupAdapter
 		
 		try
 		{
-			yser.save( new File( usersDirectory, user.getString( "relPath" ) ) );
+			yser.save( (File) user.getObject( "relPath" ) );
 		}
 		catch ( IOException e )
 		{
@@ -70,7 +107,7 @@ public class FileAdapter implements UserLookupAdapter
 		if ( user == null || !user.containsKey( "relPath" ) )
 			return user;
 		
-		YamlConfiguration yser = YamlConfiguration.loadConfiguration( new File( usersDirectory, user.getString( "relPath" ) ) );
+		YamlConfiguration yser = YamlConfiguration.loadConfiguration( (File) user.getObject( "relPath" ) );
 		
 		if ( yser == null )
 			return user;
@@ -80,31 +117,30 @@ public class FileAdapter implements UserLookupAdapter
 			user.set( key, yser.get( key ) );
 		}
 		
+		// meta.set( "displayName", ( rs.getString( "fname" ).isEmpty() ) ? rs.getString( "name" ) : rs.getString( "fname" ) + " " + rs.getString( "name" ) );
+		
 		return user;
 	}
 	
 	public UserMetaData createUser( String username, String userId ) throws LoginException
 	{
-		UserMetaData meta = new UserMetaData();
-		
 		if ( userId == null || userId.isEmpty() )
 			throw new LoginException( LoginExceptionReasons.emptyUsername );
 		
-		for ( String key : adapterConfig.getConfigurationSection( "ref" ).getKeys( false ) )
+		checkForFiles();
+		
+		for ( Entry<String, UserMetaData> e : preloaded.entrySet() )
 		{
-			ConfigurationSection section = adapterConfig.getConfigurationSection( "ref." + key );
+			UserMetaData meta = e.getValue();
 			
 			for ( String f : userFields )
-			{
-				if ( section.getString( f ) != null && ( section.getString( f ).equalsIgnoreCase( userId ) || section.getString( f ).equalsIgnoreCase( username ) ) )
-				{
+				if ( meta.getObject( f ) != null && ( meta.getString( f ).equalsIgnoreCase( userId ) || meta.getString( f ).equalsIgnoreCase( username ) ) )
 					throw new LoginException( LoginExceptionReasons.userExists );
-				}
-			}
 		}
 		
+		UserMetaData meta = new UserMetaData();
+		
 		String relPath = userId + ".yaml";
-		String acPath = "ref." + userId + ".";
 		
 		YamlConfiguration yser = new YamlConfiguration();
 		
@@ -122,9 +158,10 @@ public class FileAdapter implements UserLookupAdapter
 			throw new LoginException( e );
 		}
 		
-		adapterConfig.set( acPath + "relPath", relPath );
-		adapterConfig.set( acPath + "userId", userId.toLowerCase() );
-		adapterConfig.set( acPath + "username", username.toLowerCase() );
+		for ( String key : yser.getKeys( false ) )
+		{
+			meta.set( key, yser.get( key ) );
+		}
 		
 		return meta;
 	}
@@ -132,49 +169,29 @@ public class FileAdapter implements UserLookupAdapter
 	@Override
 	public UserMetaData loadUser( String user ) throws LoginException
 	{
-		UserMetaData meta = new UserMetaData();
-		
 		if ( user == null || user.isEmpty() )
 			throw new LoginException( LoginExceptionReasons.emptyUsername );
 		
-		ConfigurationSection found = null;
+		checkForFiles();
 		
-		for ( String key : adapterConfig.getConfigurationSection( "ref" ).getKeys( false ) )
+		UserMetaData meta = null;
+		
+		for ( Entry<String, UserMetaData> e : preloaded.entrySet() )
 		{
-			ConfigurationSection section = adapterConfig.getConfigurationSection( "ref." + key );
+			UserMetaData meta1 = e.getValue();
 			
 			for ( String f : userFields )
-			{
-				if ( section.getString( f ) != null && section.getString( f ).equalsIgnoreCase( user ) )
+				if ( meta1.getObject( f ) != null && meta1.getString( f ).equalsIgnoreCase( user ) )
 				{
-					found = section;
+					meta = meta1;
 					break;
 				}
-			}
 		}
 		
-		if ( found == null )
+		if ( meta == null )
 			throw new LoginException( LoginExceptionReasons.incorrectLogin );
 		
-		YamlConfiguration yser = YamlConfiguration.loadConfiguration( new File( usersDirectory, found.getString( "relPath" ) ) );
-		
-		if ( yser == null )
-			throw new LoginException( LoginExceptionReasons.incorrectLogin );
-		
-		// Save the file location for later
-		meta.set( "file", new File( usersDirectory, user + ".dat" ) );
-		
-		for ( String key : found.getKeys( false ) )
-		{
-			meta.set( key, found.get( key ) );
-		}
-		
-		for ( String key : yser.getKeys( false ) )
-		{
-			meta.set( key, yser.get( key ) );
-		}
-		
-		// meta.set( "displayName", ( rs.getString( "fname" ).isEmpty() ) ? rs.getString( "name" ) : rs.getString( "fname" ) + " " + rs.getString( "name" ) );
+		meta = reloadUser( meta );
 		
 		return meta;
 	}
@@ -198,7 +215,10 @@ public class FileAdapter implements UserLookupAdapter
 		if ( user == null || !user.getMetaData().containsKey( "relPath" ) )
 			return;
 		
-		File relPath = new File( usersDirectory, user.getMetaData().getString( "relPath" ) );
+		File relPath = (File) user.getMetaData().getObject( "relPath" );
+		
+		if ( relPath == null )
+			return;
 		
 		YamlConfiguration yser = YamlConfiguration.loadConfiguration( relPath );
 		
@@ -238,7 +258,10 @@ public class FileAdapter implements UserLookupAdapter
 		if ( user == null || !user.getMetaData().containsKey( "relPath" ) )
 			return;
 		
-		File relPath = new File( usersDirectory, user.getMetaData().getString( "relPath" ) );
+		File relPath = (File) user.getMetaData().getObject( "relPath" );
+		
+		if ( relPath == null )
+			return;
 		
 		YamlConfiguration yser = YamlConfiguration.loadConfiguration( relPath );
 		
