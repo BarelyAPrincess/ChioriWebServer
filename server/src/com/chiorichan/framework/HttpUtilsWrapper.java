524d0e2303bdb4f98ff6f8d317fa4a486c7cc4cd
Chiori Greene <chiorigreene@gmail.com>
2014-08-01 17:14:14 -0500

    Implemented the readUrl method. getFramework().getHttpUtils().readUrl( (url), [user], [pass] );

--- server/src/com/chiorichan/framework/HttpUtilsWrapper.java
+++ server/src/com/chiorichan/framework/HttpUtilsWrapper.java
@@ -1,11 +1,16 @@
 package com.chiorichan.framework;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 import com.chiorichan.Loader;
 import com.chiorichan.http.PersistentSession;
+import com.sun.jersey.core.util.Base64;
 
 public class HttpUtilsWrapper extends WebUtils
 {
@@ -71,5 +76,37 @@ public class HttpUtilsWrapper extends WebUtils
 		Evaling eval = sess.getEvaling();
 		
 		return evalGroovy( eval, source, filePath );
+	}
+	
+	public byte[] readUrl( String url ) throws IOException
+	{
+		return readUrl( url, null, null );
+	}
+	
+	public byte[] readUrl( String surl, String user, String pass ) throws IOException
+	{
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		URL url = new URL( surl );
+		URLConnection uc = url.openConnection();
+		if ( user != null || pass != null )
+		{
+			String userpass = user + ":" + pass;
+			String basicAuth = "Basic " + new String( new Base64().encode( userpass.getBytes() ) );
+			uc.setRequestProperty( "Authorization", basicAuth );
+		}
+		InputStream is = uc.getInputStream();
+		
+		byte[] byteChunk = new byte[4096];
+		int n;
+		
+		while ( ( n = is.read( byteChunk ) ) > 0 )
+		{
+			out.write( byteChunk, 0, n );
+		}
+		
+		is.close();
+		
+		return out.toByteArray();
 	}
 }
