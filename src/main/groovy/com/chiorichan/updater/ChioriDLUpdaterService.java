29d2eb0f268d389c2a5bf3eb9be3c6e6fe9d909e
Chiori Greene <chiorigreene@gmail.com>
2014-09-10 00:30:08 -0500

    Another quick fix to the build.gradle file

--- src/main/groovy/com/chiorichan/updater/ChioriDLUpdaterService.java
+++ src/main/groovy/com/chiorichan/updater/ChioriDLUpdaterService.java
@@ -369,7 +369,6 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.chiorichan.Loader;
 import com.chiorichan.framework.WebUtils;
 import com.google.gson.Gson;
 import com.google.gson.JsonDeserializationContext;
