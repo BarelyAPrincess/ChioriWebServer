f6e8a23d5469cbf269f0928902b9455df0ae4b13
Chiori Greene <chiorigreene@gmail.com>
2015-04-04 17:56:21 -0500

    Improved error prone initServerVars method

--- src/main/groovy/com/chiorichan/http/HttpRequestWrapper.java
+++ src/main/groovy/com/chiorichan/http/HttpRequestWrapper.java
@@ -579,41 +579,46 @@ public class HttpRequestWrapper
 		return getHeader( "User-Agent" );
 	}
 	
+	void putServerVarSafe( ServerVars key, Object value )
+	{
+		try
+		{
+			serverVars.put( key, value );
+		}
+		catch ( Exception e )
+		{
+			
+		}
+	}
+	
 	void initServerVars( Map<ServerVars, Object> staticServerVars )
 	{
 		// Not sure of the need to do a try... catch.
 		// Instead of a blanket catch, we should make this more of a personal check for each put.
-		try
-		{
-			serverVars = staticServerVars;
-			serverVars.put( ServerVars.DOCUMENT_ROOT, getSite().getAbsoluteRoot( null ) );
-			serverVars.put( ServerVars.HTTP_ACCEPT, getHeader( "Accept" ) );
-			serverVars.put( ServerVars.HTTP_USER_AGENT, getUserAgent() );
-			serverVars.put( ServerVars.HTTP_CONNECTION, getHeader( "Connection" ) );
-			serverVars.put( ServerVars.HTTP_HOST, getLocalHost() );
-			serverVars.put( ServerVars.HTTP_ACCEPT_ENCODING, getHeader( "Accept-Encoding" ) );
-			serverVars.put( ServerVars.HTTP_ACCEPT_LANGUAGE, getHeader( "Accept-Language" ) );
-			serverVars.put( ServerVars.HTTP_X_REQUESTED_WITH, getHeader( "X-requested-with" ) );
-			serverVars.put( ServerVars.REMOTE_HOST, getRemoteHost() );
-			serverVars.put( ServerVars.REMOTE_ADDR, getRemoteAddr() );
-			serverVars.put( ServerVars.REMOTE_PORT, getRemotePort() );
-			serverVars.put( ServerVars.REQUEST_TIME, getRequestTime() );
-			serverVars.put( ServerVars.REQUEST_URI, getURI() );
-			serverVars.put( ServerVars.CONTENT_LENGTH, getContentLength() );
-			// serverVars.put( ServerVars.AUTH_TYPE, getAuthType() );
-			serverVars.put( ServerVars.SERVER_NAME, getServerName() );
-			serverVars.put( ServerVars.SERVER_PORT, getServerPort() );
-			serverVars.put( ServerVars.HTTPS, isSecure() );
-			serverVars.put( ServerVars.SESSION, getSession() );
-			serverVars.put( ServerVars.SERVER_SOFTWARE, Versioning.getProduct() );
-			serverVars.put( ServerVars.SERVER_VERSION, Versioning.getVersion() );
-			serverVars.put( ServerVars.SERVER_ADMIN, Loader.getConfig().getString( "server.admin", "webmaster@" + getDomain() ) );
-			serverVars.put( ServerVars.SERVER_SIGNATURE, Versioning.getProduct() + " Version " + Versioning.getVersion() );
-		}
-		catch ( Exception e )
-		{
-			e.printStackTrace();
-		}
+		serverVars = staticServerVars;
+		putServerVarSafe( ServerVars.DOCUMENT_ROOT, getSite().getAbsoluteRoot( null ) );
+		putServerVarSafe( ServerVars.HTTP_ACCEPT, getHeader( "Accept" ) );
+		putServerVarSafe( ServerVars.HTTP_USER_AGENT, getUserAgent() );
+		putServerVarSafe( ServerVars.HTTP_CONNECTION, getHeader( "Connection" ) );
+		putServerVarSafe( ServerVars.HTTP_HOST, getLocalHost() );
+		putServerVarSafe( ServerVars.HTTP_ACCEPT_ENCODING, getHeader( "Accept-Encoding" ) );
+		putServerVarSafe( ServerVars.HTTP_ACCEPT_LANGUAGE, getHeader( "Accept-Language" ) );
+		putServerVarSafe( ServerVars.HTTP_X_REQUESTED_WITH, getHeader( "X-requested-with" ) );
+		putServerVarSafe( ServerVars.REMOTE_HOST, getRemoteHost() );
+		putServerVarSafe( ServerVars.REMOTE_ADDR, getRemoteAddr() );
+		putServerVarSafe( ServerVars.REMOTE_PORT, getRemotePort() );
+		putServerVarSafe( ServerVars.REQUEST_TIME, getRequestTime() );
+		putServerVarSafe( ServerVars.REQUEST_URI, getURI() );
+		putServerVarSafe( ServerVars.CONTENT_LENGTH, getContentLength() );
+		// putServerVarSafe( ServerVars.AUTH_TYPE, getAuthType() );
+		putServerVarSafe( ServerVars.SERVER_NAME, getServerName() );
+		putServerVarSafe( ServerVars.SERVER_PORT, getServerPort() );
+		putServerVarSafe( ServerVars.HTTPS, isSecure() );
+		putServerVarSafe( ServerVars.SESSION, getSession() );
+		putServerVarSafe( ServerVars.SERVER_SOFTWARE, Versioning.getProduct() );
+		putServerVarSafe( ServerVars.SERVER_VERSION, Versioning.getVersion() );
+		putServerVarSafe( ServerVars.SERVER_ADMIN, Loader.getConfig().getString( "server.admin", "webmaster@" + getDomain() ) );
+		putServerVarSafe( ServerVars.SERVER_SIGNATURE, Versioning.getProduct() + " Version " + Versioning.getVersion() );
 	}
 	
 	public Map<ServerVars, Object> getServerVars()
