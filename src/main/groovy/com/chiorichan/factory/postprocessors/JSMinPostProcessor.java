4dd5428c0cb6aeac0161c2be2e6d6a07a111ffa4
Chiori Greene <chiorigreene@gmail.com>
2015-04-06 22:26:52 -0500

    JSMinPostProcessor now ignores files with .min. in their name

--- src/main/groovy/com/chiorichan/factory/postprocessors/JSMinPostProcessor.java
+++ src/main/groovy/com/chiorichan/factory/postprocessors/JSMinPostProcessor.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.chiorichan.Loader;
 import com.chiorichan.factory.EvalMetaData;
 import com.google.common.collect.Lists;
 import com.google.javascript.jscomp.CompilationLevel;
@@ -36,6 +37,10 @@ public class JSMinPostProcessor implements PostProcessor
 	@Override
 	public ByteBuf process( EvalMetaData meta, ByteBuf buf ) throws Exception
 	{
+		// A simple way to ignore JS files that might already be minimized
+		if ( meta.fileName != null && meta.fileName.contains( ".min." ) )
+			return buf;
+		
 		String code = buf.toString( Charset.defaultCharset() );
 		List<SourceFile> externs = Lists.newArrayList();
 		List<SourceFile> inputs = Arrays.asList( SourceFile.fromCode( ( meta.fileName == null || meta.fileName.isEmpty() ) ? "fakefile.js" : meta.fileName, code ) );
@@ -47,6 +52,7 @@ public class JSMinPostProcessor implements PostProcessor
 		CompilationLevel.SIMPLE_OPTIMIZATIONS.setOptionsForCompilationLevel( options );
 		
 		compiler.compile( externs, inputs, options );
+		
 		return Unpooled.buffer().writeBytes( StringUtils.trimToNull( compiler.toSource() ).getBytes() );
 	}
 	
