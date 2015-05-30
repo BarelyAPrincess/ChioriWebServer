f1849ca67b307ae464d17f58ec10be2da32ca66a
Chiori Greene <chiorigreene@gmail.com>
2015-05-29 22:18:46 -0500

    Add first structure for the Interactive Console Plugin

---
+++ InteractiveConsolePlugin/src/main/java/com/chiorichan/plugin/console/ConsolePlugin.java
@@ -0,0 +1,19 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Copyright 2015 Chiori-chan. All Right Reserved.
+ */
+package com.chiorichan.plugin.console;
+
+import com.chiorichan.plugin.loader.Plugin;
+
+/**
+ * Main initializing class for the Interactive Console Plugin
+ * 
+ * @author Chiori Greene, a.k.a. Chiori-chan {@literal <me@chiorichan.com>}
+ */
+public class ConsolePlugin extends Plugin
+{
+	
+}
