17e2612ca38f2cdd8d80428a816469a087960d5f
Chiori-chan <chiorigreene@gmail.com>
2016-02-15 23:40:52 -0600

    Documentation Update

--- generateSelfSignedSSLCert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-keytool -genkey -keyalg RSA -alias selfsigned -keystore server.keystore -storetype JKS -storepass abcd1234 -validity 360 -keysize 2048
-keytool -export -alias selfsigned -keystore server.keystore -rfc -file public.crt
