23f32d50aa609c82bd5107b163e278e514d18d2c
Chiori Greene <chiorigreene@gmail.com>
2017-02-19 11:31:15 -0600

    Added new SSH Console Plugin, will replace telnet query terminal

--- SSHConsolePlugin/src/main/java/com/chiorichan/plugin/sshd/SSHPlugin.java
+++ SSHConsolePlugin/src/main/java/com/chiorichan/plugin/sshd/SSHPlugin.java
@@ -1,5 +1,50 @@
 package com.chiorichan.plugin.sshd;
 
-public class Plugin extends
+import com.chiorichan.lang.PluginException;
+import com.chiorichan.plugin.loader.Plugin;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.shell.ProcessShellFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SSHPlugin extends Plugin
 {
+	private SshServer sshd;
+
+	@Override
+	public void onDisable() throws PluginException
+	{
+		try
+		{
+			sshd.stop();
+		}
+		catch ( IOException e )
+		{
+			throw new PluginException( e );
+		}
+	}
+
+	@Override
+	public void onEnable() throws PluginException
+	{
+		try
+		{
+			sshd.start();
+		}
+		catch ( IOException e )
+		{
+			throw new PluginException( e );
+		}
+	}
+
+	@Override
+	public void onLoad() throws PluginException
+	{
+		sshd = SshServer.setUpDefaultServer();
+		sshd.setKeyPairProvider( new SimpleGeneratorHostKeyProvider( new File( getDataFolder(), "hostkey" ) ) );
+		sshd.setShellFactory( new ProcessShellFactory( new String[] {"/bin/sh", "-i", "-l"} ) );
+		sshd.setPort( 2804 );
+	}
 }
